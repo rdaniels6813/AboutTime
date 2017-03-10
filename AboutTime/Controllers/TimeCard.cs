c595033308bfd6ed8699c1a38239f4b008853b63
Ryan Daniels <rdaniels6813@gmail.com>
2017-03-09 23:41:37 -0500

    Removed default controller and added scoffolding for timecards.

--- AboutTime/Controllers/TimeCard.cs
+++ AboutTime/Controllers/TimeCard.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace AboutTime.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class TimeCard : Controller
     {
-        // GET api/values
+        // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "TimeCard1", "TimeCard2" };
         }
 
         // GET api/values/5
